9d50471ef66ef7264b16d1679e1d32dead5d3a5d
smardone <shaun.mardones@gmail.com>
2021-01-21 21:37:01 -0500

    creating base code

---
+++ src/components/project.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+function Project() {
+    const projects = [
+        {
+            name: "Project Gemini",
+            url: "https://codyrobbins99.github.io/project-gemini-forecast/index.html"
+        },
+        {
+            name: "Run Buddy",
+            url: "https://smardones.github.io/run-buddy/"
+        },
+        {
+            name: "Pair Pro",
+            url: "https://evening-shelf-38157.herokuapp.com/"
+        },
+        {
+            name: "Food Festival",
+            url: "https://smardones.github.io/food-festival/"
+        },
+        {
+            name: "Note Taker",
+            url: "https://shady-cheetah.herokuapp.com/notes"
+        },
+        {
+            name: "Weather Dashboard",
+            url: "https://smardones.github.io/weatherapp/"
+        }
+    ];
+
+    return(
+        <div>
+        <h2>Projects</h2>
+        <div className="projects-container">
+            {projects.map((project) => (
+                <a href={project.url} key={project.name}>
+                    <img
+                    src={require(`../../assets/images/${project.name.replace(/\s/g, '')}.png`)}
+                    alt={project.name}
+                    className="project-img"
+                    key={project.name}
+                    />
+                </a>
+            ))}
+        </div>
+    </div>
+    )
+}
+
+export default Project;
