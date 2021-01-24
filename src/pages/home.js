9ee79404072989f4b7cffe5fa14cddc9a5a83e23
smardone <shaun.mardones@gmail.com>
2021-01-24 18:30:09 -0500

    added css for home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,11 +3,19 @@ import React from 'react';
 
 function Home() {
     return (
-        <section>
-            <div className="jumbotron">Hi, my name is Shaun and I'm a Full Stack Web Developer</div>
-            <div className="link" id="aboutlink">About Me</div>
-            <div className="link" id="worklink">Projects</div>
-            <div className="link" id="contactlink">Contact Me</div>
+        <section className="homeContainer">
+            <div className="jumbotron">
+                <span className="hero-banner">Hi, I'm <em>Shaun</em> and I'm a <em>Full Stack Web Developer</em></span>
+            </div>
+            <div className="link" id="aboutlink">
+                <span className="link-title">About Me</span>
+            </div>
+            <div className="link" id="worklink">
+                <span className="link-title">Projects</span>
+            </div>
+            <div className="link" id="contactlink">
+                <span className="link-title">Contact Me</span>
+            </div>
         </section>
     )
 } 
