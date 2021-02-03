0591f92aee07b373133b0b84ea07891d4410f076
smardone <shaun.mardones@gmail.com>
2021-02-02 20:43:11 -0500

    added about me text

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,9 +13,10 @@ function About() {
             <div className="about-wrapper">
                 
                 <img className="self-portrait" src={self} alt="Me"/>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id nulla nec mi scelerisque tincidunt in id nulla. Fusce enim magna, tincidunt a leo et, placerat facilisis sem. Quisque et bibendum nulla. Donec metus elit, cursus quis posuere eu, laoreet ac purus. Donec et sapien at nulla lobortis suscipit non eu augue. Suspendisse vel metus commodo, tincidunt enim nec, tincidunt erat. Aenean eget enim pulvinar, lacinia tellus ut, hendrerit leo. Aenean commodo volutpat porta. Proin lacus orci, lobortis in augue at, scelerisque tristique magna. Mauris id interdum nunc, posuere porttitor nisi.
-
-                    Vestibulum commodo nisl vel purus tincidunt lobortis. Fusce tincidunt tristique quam, quis imperdiet justo semper ac. Curabitur molestie mi elit, eget faucibus sapien bibendum in. Integer fermentum sollicitudin dolor et hendrerit. Mauris tincidunt venenatis dui at euismod. Nam dapibus euismod nisi, ut scelerisque purus suscipit quis. Quisque ultricies arcu sodales, tincidunt justo eget, egestas diam. Integer ultrices faucibus est, ultricies bibendum ante gravida at. Maecenas tincidunt mauris sit amet augue dapibus placerat. In nisl metus, porta dictum dignissim sit amet, dapibus quis felis.</p>
+                <p>My name's Shaun Mardones, a full-stack web developer currently located in Central Florida who recently obtained an cerftificate of complete from the University of Central Florida’s full-stack web development bootcamp.</p> 
+                <p>With a decade in finance and customer service under my belt, I offer a unique perspective that offers flexibility, quick-thinking, and the ability to quickly pick up new concepts.</p>
+                <p>Most recently, I have put these skills to work by working on a project that allows players to track obtained items in the video game <em>Animal Crossing New Horizons</em>. I plan to continue improving this app over time as well as create new projects focusing around my love of gaming.</p>
+                <p>I am open to new work opportunities so please feel free to reach out if you would like to learn more.</p>
             </div>
             <Footer />
         </section>
